8c26eb58be754ca5b312622110ba3a182b4dbcc6
Elvin <47361106+ElvinFatalizade@users.noreply.github.com>
2019-03-26 15:44:11 +0400

    function

---
+++ HOMOWORK/js/main.js
@@ -0,0 +1,42 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+function week(day){
+switch (day) {
+    case 1:
+     console.log("1 ci gun");
+     document.write("1 ci gun");
+      break;
+    case 2: console.log("2 ci gun");
+    document.write("2 ci gun");
+      break;
+    case 3:
+    console.log("3 cu gun");
+    document.write("3 cu gun");
+      break;
+    case 4:
+    console.log("4 cu gun");
+    document.write("4 cu gun");
+      break;
+    case 5:
+    console.log("5 ci gun");
+    document.write("5 ci gun");
+      break;
+    case 6:
+    console.log("6 ci gun");
+    document.write("6 ci gun");
+      break;
+    case  7:
+    console.log("bazar");
+    document.write(" bazar");
+    default :
+    console.log("Hefte 7 gundur");
+
+  }}
+  var day= week(5);
+
+
+
+
+
+
